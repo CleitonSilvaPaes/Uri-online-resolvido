5a50ed74451d26e8771fb9d79e9ca9010b6849d9
Cleiton Paes <cleiton03122000@gmail.com>
2022-06-21 22:02:09 -0300

    Arrumado um pouco mais limpo o codigo

--- uri_1094.py
+++ uri_1094.py
@@ -1,21 +1,17 @@
-nVezes = int(input())
-dictA = {'C': 0, 'R': 0, 'S': 0}
-for i in range(nVezes):
-    qtaAnimal = input().split(' ')
-    qtaAnimal[1] = qtaAnimal[1].upper()
-    while (qtaAnimal[1] != 'C' and qtaAnimal[1] != 'R' and qtaAnimal[1] != 'S'):
-        qtaAnimal = input().split(' ').upper()
-    dictA[qtaAnimal[1]] += int(qtaAnimal[0])
-total = dictA['C'] + dictA['R'] + dictA['S']
-totalC = dictA['C']
-totalR = dictA['R']
-totalS = dictA['S']
-percentualC = (totalC / total) * 100
-percentualR = (totalR / total) * 100
-percentualS = (totalS / total) * 100
-print(f'Total: {total} cobaias')
-print(f'Total de coelhos: {totalC}')
-print(f'Total de ratos: {totalR}')
-print(f'Percentual de coelhos: {percentualC:.2f} %')
-print(f'Percentual de ratos: {percentualR:.2f} %')
-print(f'Percentual de sapos: {percentualS:.2f} %')
+# -*- coding: utf-8 -*-
+
+nVez = int(input())
+cobaias = {'C':0,'R':0,'S':0}
+for i in range(nVez):
+    nQAnimal = input().split(" ")
+    nQAnimal[0] = int(nQAnimal[0])
+    nQAnimal[1] = nQAnimal[1].upper()
+    if nQAnimal[1] == 'C' or nQAnimal[1] == 'R' or nQAnimal[1] == 'S':
+        cobaias[nQAnimal[1]] += nQAnimal[0]
+print(f'Total: {sum(cobaias.values())} cobaias')
+print(f'Total de coelhos: {cobaias["C"]}')
+print(f'Total de ratos: {cobaias["R"]}')
+print(f'Total de sapos: {cobaias["S"]}')
+print(f'Percentual de coelhos: {((cobaias["C"]) / sum(cobaias.values())) * 100:.2f} %')
+print(f'Percentual de ratos: {((cobaias["R"]) / sum(cobaias.values())) * 100:.2f} %')
+print(f'Percentual de sapos: {((cobaias["S"]) / sum(cobaias.values())) * 100:.2f} %')
