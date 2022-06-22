6f87cb1a5aac0ff30d080a17b5094394c4c561c9
Cleiton Paes <cleiton03122000@gmail.com>
2022-06-22 20:04:36 -0300

    Finalizado 1097

--- 1097/script.js
+++ 1097/script.js
@@ -1,15 +1,33 @@
-x = 7
-y = 4
-for (let i = 1; i < 10; i++) {
-    iniFin(i, x, y)
-    i++
-    x+=2
-    y+=2
-}
-
-function iniFin(i, x, y) {
-    for (let j = x; j > y; j--) {
-        document.writeln(`I=${i} J=${j}`)
-        document.write("<br/>")
+// let i = 0
+// let j = 1
+// acrecimo = (0.2)
+// while (i<=2) {
+//     for (let c = 1; c < 4; c++) {
+//         if (i > 2.19) {
+//             console.log(`I=${2} J=${j.toFixed(0)}`)
+//         }
+//         else if (i == 1.0 || i ==0.0 || i > 1.8){
+//             console.log(`I=${i.toFixed(0)} J=${j.toFixed(0)}`)
+//         } else {
+//             console.log(`I=${i.toFixed(1)} J=${j.toFixed(1)}`)
+//         }
+//     }
+//     i += acrecimo
+//     j = 1+i
+// }
+let j = 1
+i = 0
+while(i < 2) {
+    for (let c = 1; c < 4; c++) {
+        if (i > 2){
+            console.log(2, j)
+        } else if (i == 1.0 || i == 0.0 || i > 1.8) {
+            console.log(i.toFixed(0), j.toFixed(0))
+        } else {
+            console.log(i.toFixed(1), j.toFixed(1))
+        }
+        j++
     }
+    i += (0.2)
+    j = 1 + i
 }
